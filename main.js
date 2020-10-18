ed2c16765196134d7ac3ac2aeebcec778a546496
yaneyakulik <yaneyakulik@mail.ru>
2020-10-19 01:36:36 +0400

    Add green text

---
+++ main.js
@@ -0,0 +1,114 @@
+// const zalupa = num => {
+//     for( var i = 1; i <= num; i++) {
+//         if (i % 3===0 && i % 5 === 0) {
+//             console.log("FizzBuzz")
+//         } else if (i % 3===0) {
+//             console.log('fizz')
+//         } else if (i % 5===0) {
+//             console.log("buzz")
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+
+// for (let n = 1; n <= 100; n++) {
+//     let output = "";
+//     if (n % 3 == 0) output += "Fizz";
+//     if (n % 5 == 0) output += "Buzz";
+//     console.log(output || n);
+//   }
+
+
+//   for(i = 1; i <=7; i++) {
+//       let count = "";
+//       if(i.length <7) count += "#";
+
+//       console.log(count.length)
+//   }
+
+
+let page = document.querySelector(".page");
+let themeButton = document.querySelector(".themeButton");
+
+themeButton.onclick = function() {
+    page.classList.toggle("light-theme");
+      page.classList.toggle("dark-theme");
+  }
+
+  
+  
+
+let enter = document.getElementById("enterTarget");
+let acceptButton = document.getElementById("acceptButton");
+let deleteButton = document.getElementById("deleteButton");
+var qwe = document.querySelector(".ul");
+
+
+  
+ acceptButton.onclick = function() {
+        // var ull = document.createElement("li");
+    var addTarget = document.getElementById("target").value;
+      if (addTarget) {
+        enter.textContent = "Добавлено";
+        function changeText() {
+        enter.textContent="Введите задачу";
+   }   
+        setTimeout(changeText, 1000); 
+          var ull = document.createElement("li");
+          ull.innerHTML = addTarget;
+          document.querySelector(".ul").appendChild(ull);   
+          var defaultInput = document.getElementById("target").value = "";
+    }
+      ull.onclick = function() {
+        ull.classList.toggle("green-text")
+        // ull.style.setProperty("color", "green");
+        // ull.style.textDecoration = "line-through";
+        //    ull.onclick = function() {
+        //     ull.style.setProperty("color", "black");
+        //     ull.style.textDecoration = "none";
+        //   }
+      }
+        let button = document.createElement("button");
+        // button.setAttribute("id", "dynamicButton");
+        button.innerHTML = "delete";
+        ull.appendChild(button);
+        // document.getElementById("dynamicButton").style.setProperty("background-color", "black");
+        // dynamicButton.style.setProperty("color", "white");
+        button.addEventListener('click',function(){
+           this.parentNode.remove();
+       });
+    }
+ 
+
+
+// let completed = document.querySelector(www.ull);
+//   completed.onclick = function() {
+//     completed.style.setProperty("color", "green");
+//   }
+  q
+ 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // let pososi = document.getElementById("sosi");
+    //   pososi.onclick = function() {
+    //     pososi.style.setProperty("color", "white");
+    //   }
+
+      
+    
+
